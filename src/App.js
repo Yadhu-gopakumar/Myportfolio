908b75aaa1489104785be915b84fdece3c796f8e
Yadhu-gopakumar <yadhugopakumar128@gmail.com>
2024-02-12 00:47:27 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -4,15 +4,39 @@ import Projects from './components/pages/projects/Projects';
 import Contact from './components/pages/contact/Contact';
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/navbar/Footer';
+import "./components/Loader.css"
+import { useEffect, useState } from 'react';
 const App = () => {
+  const [screenLoading, setScreenLoading] = useState(false);
+  useEffect(() => {
+    setScreenLoading(true);
+    setTimeout(() => {
+      setScreenLoading(false);
+    }, 1300);
+  }, []);
+
+  const Loader = () => {
+    return (
+      <div className='preloaderbody'>
+        <div className='circle'></div>
+      </div>
+    );
+  }
+
   return (
-    <>     
-      <Navbar />
-      <Home />
-      <About />
-      <Projects />
-      <Contact />
-      <Footer />
+    <>
+      {screenLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <Navbar />
+          <Home />
+          <About />
+          <Projects />
+          <Contact />
+          <Footer />
+        </>
+      )}
     </>
   );
 }
