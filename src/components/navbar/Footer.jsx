5ffbe72feba26f0758a07894f52dc44d088936a8
Yadhu gopakumar <87005083+Yadhu-gopakumar@users.noreply.github.com>
2025-03-12 13:49:45 +0530

    Update Footer.jsx

--- src/components/navbar/Footer.jsx
+++ src/components/navbar/Footer.jsx
@@ -6,9 +6,9 @@ const Footer = () => {
         <footer className="bg-dark text-white text-center py-3">
             <p>Copyright &copy; 2024 Yadhu Gopakumar. All rights reserved.</p>
             <div>
-                <a href="/privacy-policy" className="text-white mx-2">Privacy Policy</a> | 
-                <a href="/terms-and-conditions" className="text-white mx-2">Terms & Conditions</a> | 
-                <a href="/refund-policy" className="text-white mx-2">Refund Policy</a>
+                <a href="/PrivacyPolicy.html" className="text-white mx-2">Privacy Policy</a> | 
+                <a href="/TermsAndConditions.html" className="text-white mx-2">Terms & Conditions</a> | 
+                <a href="/RefundPolic.htmly" className="text-white mx-2">Refund Policy</a>
             </div>
         </footer>
     );
