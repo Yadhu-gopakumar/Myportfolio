97dabcb39eabdced21d8bad8fcfd2a8d685b05eb
Yadhu gopakumar <87005083+Yadhu-gopakumar@users.noreply.github.com>
2025-03-12 13:39:43 +0530

    Update Footer.jsx added more links

--- src/components/navbar/Footer.jsx
+++ src/components/navbar/Footer.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import '../navbar/Navbar.css';
 
-const Footer=()=>{
-    return(
+const Footer = () => {
+    return (
         <section className='footer'>
-            Copyright &copy; 2024 Yadhu Gopakumar. All rights received.
+            <p>Copyright &copy; 2024 Yadhu Gopakumar. All rights reserved.</p>
+            <div className="footer-links">
+                <a href="/privacy-policy.html">Privacy Policy</a> | 
+                <a href="/terms-and-conditions.html"> Terms & Conditions</a> | 
+                <a href="/refund-policy.html"> Refund Policy</a>
+            </div>
         </section>
     );
-
 }
+
 export default Footer;
