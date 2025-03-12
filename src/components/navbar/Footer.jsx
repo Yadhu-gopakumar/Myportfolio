5780660d01b9f567f701d8a5f6e90bbce9c40e18
Yadhu gopakumar <87005083+Yadhu-gopakumar@users.noreply.github.com>
2025-03-12 13:41:47 +0530

    Update Footer.jsx added styles

--- src/components/navbar/Footer.jsx
+++ src/components/navbar/Footer.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import '../navbar/Navbar.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Footer = () => {
     return (
-        <section className='footer'>
+        <footer className="bg-dark text-white text-center py-3">
             <p>Copyright &copy; 2024 Yadhu Gopakumar. All rights reserved.</p>
-            <div className="footer-links">
-                <a href="/privacy-policy.html">Privacy Policy</a> | 
-                <a href="/terms-and-conditions.html"> Terms & Conditions</a> | 
-                <a href="/refund-policy.html"> Refund Policy</a>
+            <div>
+                <a href="/privacy-policy" className="text-white mx-2">Privacy Policy</a> | 
+                <a href="/terms-and-conditions" className="text-white mx-2">Terms & Conditions</a> | 
+                <a href="/refund-policy" className="text-white mx-2">Refund Policy</a>
             </div>
-        </section>
+        </footer>
     );
 }
 
